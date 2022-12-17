72212ece36c5931f3f6f24a35445138b71a4c30e
chienvn <chiennguyen1702@gmail.com>
2022-12-18 05:58:59 +0700

    GET request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Title from './components/Title';
+import FirstRequest from './examples/1-first-request';
 function App() {
   return (
     <main>
-      <Title />
+      <FirstRequest />
     </main>
   );
 }
